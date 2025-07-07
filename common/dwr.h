3e7f3991cdac60844d5b6f1cf3f5d611457504c2
Serpikmin <peter_ghob@yahoo.ca>
2025-07-07 09:58:33 -0400

    Move AP code to fix DN and FR

--- common/dwr.h
+++ common/dwr.h
@@ -9,7 +9,7 @@
 #include "build.h"
 #include "dwr_types.h"
 
-#define DWR_VERSION "ARCHIPELAGO v0.2.1"
+#define DWR_VERSION "ARCHIPELAGO v0.2.2"
 #define VERSION DWR_VERSION
 
 #define DEFAULT_FLAGS "IVIAAVCEKACAAAAAAAAAAEAUAAIBAAAAAAAAAAAA"
